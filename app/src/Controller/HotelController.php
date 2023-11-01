9c9c7e6b2cabc0b4c75f86097ebccab5214123b4
Ren Delaney <ren.delaney@gmail.com>
2023-11-01 21:04:41 +0000

    cacthing all exceptions

--- app/src/Controller/HotelController.php
+++ app/src/Controller/HotelController.php
@@ -9,30 +9,32 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Services\HotelService;
 use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Exception;
 
+#[Route('/hotel', name: 'hotel_')]
 class HotelController extends AbstractController
 {
     public function __construct(
-        protected HotelService $service
+        protected HotelService $hotelService
     ) {
     }
 
-    #[Route('/api/hotel/availability', methods: ['POST'], name: 'app_hotel_availability')]
+    #[Route('/availability', methods: ['POST'], name: 'availability')]
     public function availability(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
         try {
-            $hotelRequest = $this->service->validateHotelAvailabilityRequest($request, $entityManager);
-            $availability = $this->service->checkHotelAvailability($hotelRequest);
+            $hotelRequest = $this->hotelService->validateHotelAvailabilityRequest($request, $entityManager);
+            $availability = $this->hotelService->checkHotelAvailability($hotelRequest, $entityManager);
             return $this->json($availability);
         } catch (MissingMandatoryParametersException $e) {
             return $this->json([
-                'error' => 'Some mandatory parameters are missing',
+                'error' => 'MissingMandatoryParametersException',
+                'message' => 'Some mandatory parameters are missing',
                 'missing' => $e->getMissingParameters(),
             ], 400);
-        } catch (NotFoundHttpException $e) {
+        } catch (Exception $e) {
             return $this->json([
-                'error' => 'Not Found',
+                'error' => get_class($e),
                 'message' => $e->getMessage()
             ], 404);
         }
